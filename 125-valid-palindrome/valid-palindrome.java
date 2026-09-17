37140753ae79430563387201d6c7f6599b70ab00
Sushant Kumar <sushant.k052@gmail.com>
2026-09-17 20:50:18 +0530

    Time: 14 ms (28.09%) | Memory: 46.6 MB (17.01%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -4,14 +4,14 @@ class Solution {
         String str;
         str = s.replaceAll("[^a-zA-Z0-9]", "");
         str = str.toLowerCase();
-        char[] chars = str.toCharArray();
+       
 
         int left = 0;
-        int right = chars.length - 1;
+        int right = str.length()-1;
 
         while(left <= right)
         {
-            if(chars[left] != chars[right]) return false;
+            if(str.charAt(left) != str.charAt(right)) return false;
             else
             { 
                 left++;
